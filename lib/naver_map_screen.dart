9957325ffacdbc05474391e5d6966c3ceec343b6
AugustKim <August@hmm-dev-macbook.local>
2024-03-10 17:30:55 +0900

    네이버 맵 지도 위에 리스트뷰 슬라이드 패널 장착

--- lib/naver_map_screen.dart
+++ lib/naver_map_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:sliding_up_panel2/sliding_up_panel2.dart';
 
 class NaverMapScreen extends StatefulWidget {
   const NaverMapScreen({super.key});
@@ -13,28 +14,81 @@ class _NaverMapScreenState extends State<NaverMapScreen> {
   final onMarkerInfoWindow = NInfoWindow.onMarker(id: "test", text: "카페지오");
   final infoWindow = NInfoWindow.onMap(
       id: "test", position: NLatLng(37.5267768, 127.040659), text: "인포윈도우 텍스트");
+  late NaverMapController mapController;
+
+  late final ScrollController scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController = ScrollController();
+  }
+
+  Widget WidgetNaverMapView() {
+    return NaverMap(
+      options: const NaverMapViewOptions(locationButtonEnable: true),
+      onMapReady: (controller) {
+        mapController = controller;
+        print('map ready');
+        controller.addOverlay(marker);
+        marker.openInfoWindow(onMarkerInfoWindow);
+        // controller.addOverlay(infoWindow);
+        marker.setOnTapListener((NMarker marker) {
+          print("마커가 터치되었습니다. id: ${marker.info.id}");
+        });
+      },
+    );
+  }
+
+  Widget scrollingListView() {
+    return Column(
+      children: [
+        Container(
+          alignment: Alignment.center,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // 회색으로 정중앙에 맞춰서 만들어서 넣기
+              Container(
+                width: 30,
+                height: 5,
+                decoration: BoxDecoration(
+                    color: Colors.grey[700],
+                    borderRadius: BorderRadius.all(Radius.circular(12.0))),
+              ),
+            ],
+          ),
+        ),
+        Divider(
+          color: Colors.grey,
+        ),
+        Container(
+          child: Flexible(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return Text('$index');
+              },
+              itemCount: 50,
+              controller: scrollController,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    late NaverMapController mapController;
     return Scaffold(
       appBar: AppBar(
         title: Text('Map'),
       ),
-      body: Container(
-        child: NaverMap(
-          options: const NaverMapViewOptions(locationButtonEnable: true),
-          onMapReady: (controller) {
-            mapController = controller;
-            print('map ready');
-            controller.addOverlay(marker);
-            marker.openInfoWindow(onMarkerInfoWindow);
-            // controller.addOverlay(infoWindow);
-            marker.setOnTapListener((NMarker marker) {
-              print("마커가 터치되었습니다. id: {$marker.id}");
-            });
-          },
-        ),
+      body: SlidingUpPanel(
+        body: WidgetNaverMapView(),
+        scrollController: scrollController,
+        panelBuilder: () {
+          return scrollingListView();
+        },
       ),
     );
   }
